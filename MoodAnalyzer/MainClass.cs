ca0475f1855446179310619f757ad8da0643fe0b
KUMMARI TIRUPATHI RAO <35338095+ktr-hub@users.noreply.github.com>
2020-10-12 20:27:01 +0530

    [Tirupathi] Add. Responding Happy or Sad Mood in UC1

--- MoodAnalyzer/MainClass.cs
+++ MoodAnalyzer/MainClass.cs
@@ -6,7 +6,7 @@ namespace MoodAnalyzer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Mood Analyzer Problem");
+            Console.WriteLine("Welcome to Mood Analyzer Problem using Exceeption Handling");
         }
     }
 }
