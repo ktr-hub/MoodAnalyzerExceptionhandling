2774603000cccc7f8c0d97048876b7fcb03ff54d
KUMMARI TIRUPATHI RAO <35338095+ktr-hub@users.noreply.github.com>
2020-10-14 10:15:15 +0530

    [Tirupathi] Add. Implemented Reflection concept to get an object of class having parameterized constructor

--- MoodAnalyzer/MoodAnalyserFactory.cs
+++ MoodAnalyzer/MoodAnalyserFactory.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Mime;
 using System.Reflection;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -9,6 +10,28 @@ namespace MoodAnalyzer
 {
     public class MoodAnalyserFactory
     {
+        public static object CreateMoodAnalyseUsingParameterized(string className, string constructorName)
+        {
+            Type type = typeof(MoodAnalyzerClass);
+
+            if (type.Name.Equals(className)|| type.FullName.Equals(className))
+            {
+                if(type.Name.Equals(constructorName))
+                {
+                    ConstructorInfo ctor = type.GetConstructor(new[] { typeof(string) });
+                    object instance = ctor.Invoke(new object[] { "HAPPY" });
+                    return instance;
+                }
+                else
+                {
+                    throw new MoodAnalyserCustomException("Constructor is not found");
+                }
+            }
+            else
+            {
+                throw new MoodAnalyserCustomException("Class is not found");
+            }
+        }
         public static object CreateMoodAnalyse(string className,string constructorName)
         {
             string pattern = @"." + constructorName + "$";
--- UnitTestProject1/MoodAnalyzerMsTestClass.cs
+++ UnitTestProject1/MoodAnalyzerMsTestClass.cs
@@ -7,15 +7,15 @@ namespace MoodAnalyzerMsTest
     public class MoodAnalyzerMsTestClass
     {
         [TestMethod]
-        [DataRow (null)]
-        [DataRow ("")]
+        [DataRow(null)]
+        [DataRow("")]
         public void TestMethod1(string messages)
         {
             string expected = "Exception caught : Null encountered";
             string message = messages;
             MoodAnalyzerClass moodAnalyzer = new MoodAnalyzerClass(message);
             string result = moodAnalyzer.AnalyzerMethod();
-            Assert.AreEqual(expected,result);
+            Assert.AreEqual(expected, result);
         }
 
         [TestMethod]
@@ -24,6 +24,14 @@ namespace MoodAnalyzerMsTest
             string message = null;
             object expected = new MoodAnalyzerClass(message);
             object result = MoodAnalyserFactory.CreateMoodAnalyse("MoodAnalyzer.MoodAnalyzerClass", "MoodAnalyzerClass");
+            expected.Equals(result);
+        }
+
+        [TestMethod]
+        public void GivenClassReturnsObjectParameterized()
+        {
+            object result = MoodAnalyserFactory.CreateMoodAnalyseUsingParameterized("MoodAnalyzer.MoodAnalyzerClass", "MoodAnalyzerClass");
+            object expected = new MoodAnalyzerClass("SAD");
             expected.Equals(result);
         }
 
